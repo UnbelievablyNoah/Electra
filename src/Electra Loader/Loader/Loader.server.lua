38de770e4700b60ccd7808f3ad4f19cee480aa4f
n343nc001 <noahcia343@gmail.com>
2022-03-04 16:31:34 -0500

    s

--- src/Electra Loader/Loader/Loader.server.lua
+++ src/Electra Loader/Loader/Loader.server.lua
@@ -7,11 +7,6 @@ else
 	
 	local module = game.ServerScriptService.MainModule
 	local settings = require(script.Parent.Parent.Config.Settings)
-	local data = {
-		Time = tick(); 
-		Loader = true;
-		Debug = true;
-		Settings = settings;
-	}
-	require(module)(data)
+	local data = { Time = tick(), Loader = true, Debug = true, Settings = settings }
+	require(module(data))
 end
